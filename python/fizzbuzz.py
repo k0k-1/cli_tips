c85da4548388cb88c7f2c20fbe1d26fa72b31564
k0k-1 <k@k0.gg>
2017-05-06 16:38:57 +0900

    [python] add command line arguments

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -3,7 +3,11 @@
 
 import sys
 
-for n in range(1, 32+1):
+if (len(sys.argv) != 2):
+    print ("usage: $ python", sys.argv[0], "number")
+    quit()
+
+for n in range(1, int(sys.argv[1])+1):
     if n % 3 == 0:
         print ("fizz", end="")
     if n % 5 == 0:
