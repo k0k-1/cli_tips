92e4b8932da921591058dedf8d4dbd50c072885f
k0k-1 <k@k0.gg>
2017-05-14 19:49:15 +0900

    [python] fix output

--- python/cheep/loop.py
+++ python/cheep/loop.py
@@ -1,11 +1,10 @@
 import time
-
 start = time.time()
-L = 200000
+loop = 10000
 total = 0
-for i in range(L):
-    for j in range(L):
+for i in range(loop):
+    for j in range(loop):
         total += i*j
 print (total)
 t_time = time.time() - start
-print (time, "[sec]")
+print (t_time, "[sec]")
