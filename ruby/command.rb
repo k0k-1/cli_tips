88167696716346a73650d7364a891ecdbbe2a42f
k0k-1 <k@k0.gg>
2018-04-02 20:45:17 +0900

    [msg] Large change

--- ruby/command.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-print "> "
-cmd = gets.chomp
-syscmd = system( "#{cmd}" )
-print syscmd
--- shell/pa.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -u
-trap exit ERR
-
-ip=192.168.0.$1
-
-ping -c 1 -W 1 ${ip} && arp -a ${ip} >> arp.log
